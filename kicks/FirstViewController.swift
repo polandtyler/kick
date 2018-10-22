a065b33e6077cb02b47a9b4fe4ead1fdd06927ad
Tyler Poland <poland.tyler@gmail.com>
2018-10-21 19:28:58 -0500

    Initial Commit

---
+++ kicks/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  kicks
+//
+//  Created by Tyler Poland on 10/21/18.
+//  Copyright © 2018 Tyler Poland. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ kicks/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  kicks
+//
+//  Created by Tyler Poland on 10/21/18.
+//  Copyright © 2018 Tyler Poland. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
